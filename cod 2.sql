9c62376b0246d6323f5a9361389aabeda7c22107
Balázs Tóth <gawnoris@gmail.com>
2020-02-18 19:37:42 +0100

    Add files via upload

--- cod 2.sql
+++ cod 2.sql
@@ -15,7 +15,19 @@ SELECT   hr.countries.country_name as "asd" FROM HR.countries;
 select 1+1 from dual;
 --listázza ki()[]{}<>
 --listázza ki az országok neveit és kódjaikat a hr.countries táblából
-SELECT   hr.countries.country_name as "names",country_id as "IDs" FROM HR.countries;
---listázza ki
+SELECT   hr.countries.country_name as "names",country_id  IDs FROM HR.countries;
 
---listázza ki
+--listázza ki az országok régiókódjait a HR.countries tábálból
+select hr.countries.region_id as "régió-kód" from HR.countries GROUP BY region_id;-- order by region_id;
+select DISTINCT hr.countries.region_id as "régió-kód" from HR.countries;
+select unique hr.countries.region_id as "régió-kód" from HR.countries;
+
+--listázza ki a dolgozók vezetéknevét és keresztnevét az employees táblából
+SELECT
+    hr.employees.last_name,first_name
+FROM HR.employees;
+
+--listázzuk ki a hr employees táblában szereplõ adatok alapján a dolgozók teljes nevét
+SELECT
+    last_name|| ' ' ||first_name as "teljes név" 
+FROM HR.employees;
