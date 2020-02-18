6e8a586dadb5152f61fa8cdbe3ad64fe18618d2d
Balázs Tóth <gawnoris@gmail.com>
2020-02-18 18:53:22 +0100

    Add files via upload

---
+++ cod 2.sql
@@ -0,0 +1,9 @@
+--listázza ki a hr.countries tábla adatait
+SELECT * FROM HR.countries;
+/*asd
+asd*/
+--listázza ki a hr.departments tábla adatait
+SELECT hr.departments.* FROM HR.departments;
+
+--listázza ki a hr.employees tábla adatait
+SELECT hr.employees.* FROM HR.employees;
